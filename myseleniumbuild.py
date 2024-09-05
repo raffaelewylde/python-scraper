33d93d3c22b804e4205f18ab2895f4ab03add168
Raffaele Wylde <raffaelewylde@gmail.com>
2024-09-05 18:58:37 -0400

    looks like its working

--- myseleniumbuild.py
+++ myseleniumbuild.py
@@ -18,11 +18,11 @@ import requests
 
 BASE_URL = "https://learnenough.com"
 MID_URL = "/courses/"
-MID_URL_LONG = "/courses/learn_enough_"
+MID_URL_LONG = "/course/learn_enough_"
 COURSE_URL_ENDINGS = ["command_line", "text_editor", "git", "html", "action_cable"]
 ADDITIONAL_COURSE_URLS = ["css_and_layout", "javascript", "python", "ruby", "ruby_on_rail_tutorial_7th_edition", "ruby_on_rail_tutorial_6th_edition", "ruby_on_rail_tutorial_4th_edition"]
 FULL_COURSE_URLS = [f"{BASE_URL}{MID_URL_LONG}{COURSE_URL_ENDING}" for COURSE_URL_ENDING in COURSE_URL_ENDINGS]
-FULL_ADDITIONAL_COURSE_URLS = [f"{BASE_URL}{MID_URL_LONG}{ADDITIONAL_COURSE_URL}" for ADDITIONAL_COURSE_URL in ADDITIONAL_COURSE_URLS]
+FULL_ADDITIONAL_COURSE_URLS = [f"{BASE_URL}{MID_URL}{ADDITIONAL_COURSE_URL}" for ADDITIONAL_COURSE_URL in ADDITIONAL_COURSE_URLS]
 ALL_COURSE_URLS = FULL_COURSE_URLS + FULL_ADDITIONAL_COURSE_URLS
 LOGIN_URL = f"{BASE_URL}/login"
 DOWNLOAD_DIR = "my_selenium_downloads"
@@ -31,6 +31,7 @@ VIDEO_URLS = []
 PASSWORD = "ham8yhm!RXJ3xqm2enc"
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
+print(f"Welcome to The LearnEnough Course Downloader! We're using a list of courses: {ALL_COURSE_URLS}")
 
 def login():
     """
@@ -53,23 +54,22 @@ def login():
         driver.find_element(By.ID, "user_login").send_keys(LOGIN)
         driver.find_element(By.ID, "user_password").send_keys(PASSWORD)
         driver.find_element(By.NAME, "commit").click()
-        WebDriverWait(driver, 20).until(
-            EC.presence_of_element_located(LOGIN_CHECK_ELEMENT)
-        )
-        print("Login successful!")
+        time.sleep(5)
     except TimeoutException:
-        print("Login failed.")
-def is_logged_in():
-    """
-    Checks if the user is logged in by looking for the presence of the "Log Out" link.
-    Returns True if the "Log Out" link is found, False otherwise.
-    global driver"""
-    try:
-        driver.find_element(By.ID, LOGIN_CHECK_ELEMENT)
-        return True
-    except:
-        return False
-    
+        print("Timed out waiting for the login form to load.")
+
+#def is_logged_in():
+#    LOGIN_CHECK_ELEMENT = driver.find_element(By.CSS_SELECTOR, ".option-check.subscribed")
+#    """
+#    Checks if the user is logged in by looking for the presence of the "Log Out" link.
+#   Returns True if the "Log Out" link is found, False otherwise.
+#   global driver"""
+#    try:
+#        LOGIN_CHECK_ELEMENT.is_displayed()
+#        return True
+#    except:
+#        return False
+#    
 def download_file(url, download_path):
     """
     Downloads a file from the given URL and saves it to the specified download_path.
@@ -113,34 +113,34 @@ def get_video_urls():
     This function should return a list of video URLs found on the current page.
     :return: A list of video URLs
     """
-    try:
-        video_elements = driver.find_elements(By.TAG_NAME, "video")
-        for video_element in video_elements:
-            video_source = video_element.get_attribute("source")
+    #  try:
+    #     video_element = driver.find_elements(By.ID, "vjs_video_3")
+    #    video_source = video_element.get_attribute("source")
 
-            video_url = video_source.get_attribute("src")
-            if video_url:
-                print(f"Video URL: {video_url} added to video URLs list.")
+#        if video_source:
+#            video_url = video_source.get_attribute("src")
+#            if video_url:
+#                print(f"Video URL: {video_url} added to video URLs list.")
+#                VIDEO_URLS.append(video_url)
+#        return VIDEO_URLS
+#    except:
+#        print("No video elements found on the page.")
+    try:
+        source = driver.page_source
+        soup = BeautifulSoup(source, "html.parser")
+        video_src_tag = soup.find("source", string=re.compile(r"mp4"))
+        if video_src_tag:
+            tag_content = video_src_tag.string
+            link_match = re.search(r"^http://.*cloudfront.net.*[a-Z0-9]{20}$", tag_content)
+            if link_match:
+                print(link_match)
+                print(link_match.group(0))
+                print(link_match.group(1))
+                video_url = link_match.group(1)
+                print(f"Found Video URL: {video_url} added to video URLs list.")
                 VIDEO_URLS.append(video_url)
-        return VIDEO_URLS
     except NoSuchElementException:
         print("No video elements found on the page.")
-        try:
-            source = driver.page_source
-            soup = BeautifulSoup(source, "html.parser")
-            video_src_tag = soup.find("source", string=re.compile(r"mp4"))
-            if video_src_tag:
-                tag_content = video_src_tag.string
-                link_match = re.search(r"^http://.*cloudfront.net.*[a-Z0-9]{20}$", tag_content)
-                if link_match:
-                    print(link_match)
-                    print(link_match.group(0))
-                    print(link_match.group(1))
-                    video_url = link_match.group(1)
-                    print(f"Found Video URL: {video_url} added to video URLs list.")
-                    VIDEO_URLS.append(video_url)
-        except NoSuchElementException:
-            print("No video elements found on the page.")
 def page_actions():
     current_url = driver.current_url
     download_page_content(current_url)
@@ -152,13 +152,18 @@ def page_actions():
 
 def main():
     login()
-    LOGIN_CHECK_ELEMENT = driver.find_element(By.CSS_SELECTOR, "a[href='/sign_out']")
+    #LOGIN_CHECK_ELEMENT = driver.find_element(By.CSS_SELECTOR, "a[href='/sign_out']")
     for courseurl in ALL_COURSE_URLS:
-        if not is_logged_in():
-            login()
+        print(f"Attempting to navigate to {courseurl}...")
+        #if not is_logged_in():
+        #    login()
         driver.get(courseurl)
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "btn.btnSmall")))
-        driver.find_element(By.CLASS_NAME, "btn.btnSmall").click()
+        attention_button = driver.find_element(By.CLASS_NAME, "btn.attentiond")
+        next_button = driver.find_element(By.CLASS_NAME, "btn.btnSmall")
+        if next_button.is_displayed():
+            next_button.click()
+        elif attention_button.is_displayed():
+            continue
         time.sleep(5)
         while True:
             try:
